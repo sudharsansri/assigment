328a2e5886fc68109b0a9a9f7c39d2ede9131f86
sudharsansri <121850694+sudharsansri@users.noreply.github.com>
2023-08-24 17:32:27 +0530

    Create html

---
+++ html.js
@@ -0,0 +1,86 @@
+<html>
+
+<head> </head>
+
+<body>
+    <div class="bg">
+        <h1 class="heading">Bundle and save</h1>
+        <div class="container" id="main">
+
+            <div class="content">
+                <div class="row">
+                    <input class="rad" type="radio" onclick="changecolor()">
+                    <div>
+                        <p class="head">1 Pair</p>
+                        <p class="head">DKK 195.</p>
+                    </div>
+                </div>
+                <p class="p2">50% OFF</p>
+            </div>
+        </div>
+
+        <div class="container" id="main2">
+
+            <div class="content">
+                <div class="row">
+                    <input class="rad" type="radio" onclick="change()">
+                    <div>
+                        <p class="head">2 Pair</p>
+                        <p class="head">DKK 345.00</p>
+                        <div class="a1">
+                            <div>
+                                <p>size</p>
+                                <select>
+
+                                    <option>Hello</option>
+
+                                </select>
+
+                            </div>
+                            <div>
+                                <p>Color</p>
+                                <select>
+
+                                    <option>color</option>
+
+                                </select>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p class="text">DKK 105.00</p>
+                </div>
+                <p class="p2">50% OFF</p>
+            </div>
+        </div>
+
+
+        <div class="container" id="main3">
+
+            <div class="content">
+                <div class="row">
+                    <input class="rad" type="radio" onclick="change2()">
+                    <div>
+                        <p class="head">1 Pair</p>
+                        <p class="head">DKK 195.00</p>
+                    </div>
+                </div>
+                <p class="p2">50% OFF</p>
+            </div>
+        </div>
+       <div class="m4">
+        <div class="content">
+            <p>Free 2 Day Shipping</p>
+            <P>Total: DKK 360.00</P>
+        </div>
+        </div>
+
+        <button class="button">ADD TO CART</button>
+         <P>@powered by pumber</P>
+    </div>
+
+</body>
+
+</html>
